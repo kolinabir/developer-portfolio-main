95c8a0d06859fabb176080915843dba7c54a3d8b
Kolinabir <knkolin9@gmail.com>
2024-05-22 04:30:38 +0600

    Updating file

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,7 +1,7 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
+    title: 'Software Engineer Boss I',
     company: "Teton Private Ltd.",
     duration: "(Jan 2022 - Present)"
   },
