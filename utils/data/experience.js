55e55e023c1e8f88ecdcc2607c743953c945fab5
Kolinabir <knkolin9@gmail.com>
2024-05-31 22:00:22 +0600

    Updating experience file

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,8 +1,14 @@
 export const experiences = [
   {
     "id": 1,
-    "title": "Software EngineerXX@$",
+    "title": "Software Engineer",
     "company": "Acme Inc.",
     "duration": "Jan 2023 - Present"
+  },
+  {
+    "id": 2,
+    "title": "Intern",
+    "company": "TechCo",
+    "duration": "May 2022 - Aug 2022"
   }
 ];
