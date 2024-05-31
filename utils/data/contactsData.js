b8409e31a45e114bda200c85ed1fc09481fe25e9
Kolinabir <knkolin9@gmail.com>
2024-05-31 21:29:18 +0600

    Updating file

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'john.doe@example.com',
-    phone: '+1 123-456-7890',
-    address: '123 Main St, Anytown USA',
-    github: 'https://github.com/johndoe',
-    facebook: 'https://www.facebook.com/johndoe',
-    linkedIn: 'https://www.linkedin.com/in/johndoe', // Note the change from 'linkedin' to 'linkedIn'
-    twitter: 'https://twitter.com/johndoe',
-    stackOverflow: 'https://stackoverflow.com/users/1234567/johndoe',
-    devUsername: 'johndoe'
+    email: 'undefined',
+    phone: 'undefined',
+    address: 'undefined',
+    github: 'undefined',
+    facebook: 'undefined',
+    linkedIn: 'undefined',
+    twitter: 'undefined',
+    stackOverflow: 'undefined',
+    devUsername: 'undefined'
   }
