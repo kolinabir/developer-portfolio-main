9b90000038592769fd60b0a7943cc79ff7eb2e1c
Kolinabir <knkolin9@gmail.com>
2024-05-31 22:00:24 +0600

    Updating personal data file

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,17 @@
 export const personalData = {
-  name: "Kolin XX4",
-  profile: '/profile.png',
-  designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-}
+  "name": "KOLINBNXXXX",
+  "profile": "https://johndoe.com/profile.jpg",
+  "designation": "Full-Stack Developer",
+  "description": "I am a passionate full-stack developer with expertise in React, Node.js, and MongoDB.",
+  "email": "john.doe@example.com",
+  "phone": "+1 123-456-7890",
+  "address": "123 Main St, Anytown USA",
+  "github": "https://github.com/johndoe",
+  "facebook": "https://www.facebook.com/johndoe",
+  "linkedIn": "https://www.linkedin.com/in/johndoe",
+  "twitter": "https://twitter.com/johndoe",
+  "stackOverflow": "https://stackoverflow.com/users/1234567/johndoe",
+  "leetcode": "https://leetcode.com/johndoe",
+  "devUsername": "johndoe",
+  "resume": "https://johndoe.com/resume.pdf"
+};
